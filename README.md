9507b674ee1ff3ec587bbd72edd6cccb8527b94d
Richard Baptist <rpbaptist@users.noreply.github.com>
2016-02-24 21:32:14 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Ubuntu set up script
 
+**WORK IN PROGRESS**
+
 Steps required:
 
-1. Create/add ssh key
-2. Install git and ansible: wget -O https://github.com/rpbaptist/ubuntu-setup/blob/master/runner.sh | sh
+1. Install git and ansible:
+
+  `wget -O https://github.com/rpbaptist/ubuntu-setup/blob/master/installer.sh | sh`
+
+2. Get this ansible playbook:
+
+  `git clone https://github.com/rpbaptist/ubuntu-setup.git ~/.ansible`
+
+3. Run ansible:
+
+  `cd ~/.ansible && ansible-playbook main.yml -i hosts`
+
--- runner.sh
+++ runner.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
-{
-    echo "This script requires superuser access to install apt packages."
-    echo "You will be prompted for your password by sudo."
-
-    # clear any previous sudo permission
-    sudo -k
-
-    # run inside sudo
-    sudo sh <<SCRIPT
-
-  # add heroku repository to apt
-  add-apt-repository ppa:git-core/ppa
-  add-apt-repository ppa:ansible/ansible
-
-  # update your sources
-  apt-get update
-
-  # install git and ansible
-  apt-get install git ansible
-
-SCRIPT
-}
-
-# Get the rest of the script
+# Get the ansible script
 git clone https://github.com/rpbaptist/ubuntu-setup.git ~/.ansible
 
-# Run ansible
+# Run ansiblell
 cd ~/.ansible && ansible-playbook main.yml -i hosts
