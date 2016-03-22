ea85794e5c51a1398d200b7c6ff43e289d8fa524
Richard Baptist <rpbaptist@gmail.com>
2016-03-22 15:48:49 +0100

    Add pulseaudio-equalizer
    Add -K parameter to runner

--- runner.sh
+++ runner.sh
@@ -2,5 +2,5 @@
 # Get the ansible script
 git clone https://github.com/rpbaptist/ubuntu-setup.git ~/.ansible
 
-# Run ansiblell
-cd ~/.ansible && ansible-playbook main.yml -i hosts
+# Run ansible
+cd ~/.ansible && ansible-playbook main.yml -i hosts -K
